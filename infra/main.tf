216e7f7e62d2bd04c5158396fa2a1de3828d7b45
CloudWithVithu <vithushanvisuvalingam@gmail.com>
2025-08-06 05:53:46 +0530

    Monitoring and Logic app get created

--- infra/main.tf
+++ infra/main.tf
@@ -50,3 +50,49 @@ resource "azurerm_windows_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_monitor_action_group" "vm_action_group" {
+  name                = "autoheal-action-group2"
+  resource_group_name = azurerm_resource_group.main.name
+  short_name          = "autohealAG2"
+
+  email_receiver {
+    name                    = "admin"
+    email_address           = "vithushanvisuvalingam@gmail.com" # Replace with your real email
+    use_common_alert_schema = true
+  }
+
+  webhook_receiver {
+    name                    = "logicapp-hook2"
+    service_uri             = "https://prod-24.centralindia.logic.azure.com:443/workflows/c4fb51be9005408d993587149dc18d71/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=cpzX3A_5LHT3WevPpngv7HIiCPWuobqs2eSBb7Ucp98"
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "high_cpu_alert" {
+  name                = "high-cpu-alert2"
+  resource_group_name = azurerm_resource_group.main.name
+  description         = "Alert when CPU usage > 90%"
+  severity            = 3
+  enabled             = true
+  frequency           = "PT1M"
+  window_size         = "PT5M"
+  scopes              = [azurerm_windows_virtual_machine.vm.id]
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 90
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.vm_action_group.id
+  }
+
+  tags = {
+    environment = "autoheal"
+  }
+}
+
